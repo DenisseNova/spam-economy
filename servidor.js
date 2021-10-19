adb063973584b24ce9c49401aa54b1dfb84267d2
Denisse Nova <denisse.nova.v@gmail.com>
2021-10-19 17:14:38 -0300

    creacion sevidor

--- servidor.js
+++ servidor.js
@@ -1,3 +1,27 @@
 const url = require('url');
 const http = require('http')
 const { v4: uuidv4 } = require('uuid');
+const { enviar } = require('./script');
+
+http
+  .createServer( (req, res) => {
+    let { correos, asunto, contenido} = url.parse(req.url, true). query
+
+    if (req.url.startsWith('/')) {
+      enviar(correos, asunto, contenido)
+      res.end('se envio correo... revisar que haya sido eviado')
+    };
+  })
+  .listen(3000)
+
+
+
+
+
+
+
+
+
+
+
+
